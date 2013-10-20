aeee7d81e296e53632f07dd13050a4ab5388bfeb
csiu <celia.sy.siu@gmail.com>
2013-10-20 15:32:36 -0700

    made it so that the ignoring is done with the prune option of find instead of grep -v

--- bash/tree2.sh
+++ bash/tree2.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 ## like tree function but has ignore capabilities using '-i' flag
-## funcitons uses grep -v
+## funcitons uses -prune option of find to do the ignoring
+## USAGE: sh <script> [-i "dir1 dir2 ..."]
 
 while getopts i: option
 do
@@ -14,13 +15,21 @@ done
 if [[ $IGNORE == "" ]]; then
     IGNORE="^$"
 else
-    IGNORE=`echo $IGNORE | sed 's/^/^.\//; s/\/$//'`
     echo "ignoring="$IGNORE
 fi
 
-find . | grep -v $IGNORE | awk -v pwd=$PWD 'BEGIN {print pwd} \
-                                          !/^\.$/ {for (i=1; i<NF; i++) { \
-                                                       printf("%4s", "|") \
-                                                       } \
-                                                   print "-- "$NF \
-                                                  }' FS='/'
+FIND_CMD=$(echo "find ." \
+                $(echo ${IGNORE} | \
+                    tr ' ' '\n'  | \
+                        while read line ;
+                        do
+                            echo "-o -path ./$line -prune";
+                        done) \
+                " -o -print" | sed 's/-o//')
+
+$FIND_CMD | awk -v pwd=$PWD 'BEGIN {print pwd} \
+                           !/^\.$/ {for (i=1; i<NF; i++) { \
+                                        printf("%4s", "|") \
+                                        } \
+                                    print "-- "$NF \
+                                   }' FS='/'
