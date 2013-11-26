a59f515d5fecb633b90f66628935ce273c49f7bd
csiu <celia.sy.siu@gmail.com>
2013-11-26 09:24:07 -0800

    added usage to template

--- bash/template/ex-getopts_flags.sh
+++ bash/template/ex-getopts_flags.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
+SCRIPT=$0
+USAGE="""Usage: <STDIN> | sh ${SCRIPT} -i INFILE [options]
+Function:
+
+Options:
+-h          print help message 
+-i INFILE   path to infile
+-x VARX     ...
+"""
 
 ## user can pass args in any order using flags
-while getopts x:y:z: option
+while getopts hi:x: option
 do
     case "${option}" in
+	h) echo "$USAGE" && exit;;
+	i) INFILE=${OPTARG};;
 	x) VARX=${OPTARG};;
-	y) VARY=${OPTARG};;
-	z) VARZ=${OPTARG};;
     esac
 done
 
+echo "i="$INFILE
 echo "x="$VARX
-echo "y="$VARY
-echo "z="$VARZ
