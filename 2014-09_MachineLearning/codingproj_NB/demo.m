2974953617b7b1e4b383cb1edb280bc8aee7ab0f
csiu <celia.sy.siu@gmail.com>
2014-12-15 17:49:17 -0800

    init demo

---
+++ 2014-09_MachineLearning/codingproj_NB/demo.m
@@ -0,0 +1,19 @@
+X = [1, 1, 1, 1;
+     1, 1, 0, 0;
+     1, 1, 2, 1;
+     1, 0, 1, 1;
+     1, 0, 0, 0;
+     1, 0, 2, 1;
+     1, 0, 2, 0;
+     1, 1, 1, 1];
+ 
+ y = [0;1;1;1;0;1;0;1];
+ 
+ Xhat = [0,1,1,0;
+         1,1,2,0];
+     
+model = matLearn_classification_generativeNB(X, y);
+yhat = model.predict(model, Xhat);
+
+
+
