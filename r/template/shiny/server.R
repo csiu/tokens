cc43de3fce6eb866004364d75be0e3dc75e1d3ea
csiu <csiu@celias-MacBook-Pro.local>
2014-12-31 17:27:14 -0800

    template for R::shiny

---
+++ r/template/shiny/server.R
@@ -0,0 +1,10 @@
+library(shiny)
+shinyServer(function(input, output) {
+  
+  # reactive expression
+  # x <- reactive({...input$IN...})
+  
+  # reactive expression in render* (or another reactive expression)
+  # output$OUT <- render*({...x()...})
+  
+})
---
+++ r/template/shiny/ui.R
@@ -0,0 +1,20 @@
+library(shiny)
+shinyUI(fluidPage(
+  
+  #   titlePanel(""),
+  #   sidebarLayout(
+  #     sidebarPanel(
+  #       # use *Input("IN") widgets here
+  #     ),
+  #     mainPanel(
+  #       # use *Output("OUT") here
+  #     )
+  #   )
+  
+  #   titlePanel(""),
+  #   fluidRow(
+  #     column(3),
+  #     column(3)
+  #   )
+  
+))
